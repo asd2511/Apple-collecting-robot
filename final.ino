afd2b45c85665f87fef7c9c7738768ecf4d3d295
asd2511 <44145693+asd2511@users.noreply.github.com>
2019-04-05 13:01:50 +0200

    Add files via upload

---
+++ final.ino
@@ -0,0 +1,191 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <MeMegaPi.h>
+
+
+//Encoder Motor
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+MeEncoderOnBoard Encoder_3(SLOT3);
+MeEncoderOnBoard Encoder_4(SLOT4);
+
+//Serial Communication
+String ser_cache;
+int serNum;
+int serial_get;
+int serSet_0;
+int serSet_1;
+int serSet_2;
+int serSet_3;
+int serSet_4;
+int serSet_5;
+
+void isr_process_encoder1(void)
+{
+      if(digitalRead(Encoder_1.getPortB()) == 0){
+            Encoder_1.pulsePosMinus();
+      }else{
+            Encoder_1.pulsePosPlus();
+      }
+}
+
+void isr_process_encoder2(void)
+{
+      if(digitalRead(Encoder_2.getPortB()) == 0){
+            Encoder_2.pulsePosMinus();
+      }else{
+            Encoder_2.pulsePosPlus();
+      }
+}
+
+void isr_process_encoder3(void)
+{
+      if(digitalRead(Encoder_3.getPortB()) == 0){
+            Encoder_3.pulsePosMinus();
+      }else{
+            Encoder_3.pulsePosPlus();
+      }
+}
+
+void isr_process_encoder4(void)
+{
+      if(digitalRead(Encoder_4.getPortB()) == 0){
+            Encoder_4.pulsePosMinus();
+      }else{
+            Encoder_4.pulsePosPlus();
+      }
+}
+
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+            leftSpeed = -speed;
+            rightSpeed = speed;
+      }else if(direction == 2){
+            leftSpeed = speed;
+            rightSpeed = -speed;
+      }else if(direction == 3){
+            leftSpeed = speed;
+            rightSpeed = speed;
+      }else if(direction == 4){
+            leftSpeed = -speed;
+            rightSpeed = -speed;
+      }
+      Encoder_1.setTarPWM(rightSpeed);
+      Encoder_2.setTarPWM(leftSpeed);
+}
+void moveDegrees(int direction,long degrees, int speed_temp)
+{
+      speed_temp = abs(speed_temp);
+      if(direction == 1)
+      {
+            Encoder_1.move(degrees,(float)speed_temp);
+            Encoder_2.move(-degrees,(float)speed_temp);
+      }
+      else if(direction == 2)
+      {
+            Encoder_1.move(-degrees,(float)speed_temp);
+            Encoder_2.move(degrees,(float)speed_temp);
+      }
+      else if(direction == 3)
+      {
+            Encoder_1.move(degrees,(float)speed_temp);
+            Encoder_2.move(degrees,(float)speed_temp);
+      }
+      else if(direction == 4)
+      {
+            Encoder_1.move(-degrees,(float)speed_temp);
+            Encoder_2.move(-degrees,(float)speed_temp);
+      }
+    
+}
+
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+double changer;
+
+Servo servo_61;
+Servo servo_60;
+
+
+
+
+void setup(){
+    //Set Pwm 8KHz
+    TCCR1A = _BV(WGM10);
+    TCCR1B = _BV(CS11) | _BV(WGM12);
+    TCCR2A = _BV(WGM21) | _BV(WGM20);
+    TCCR2B = _BV(CS21);
+    
+    attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder3, RISING);
+    Encoder_3.setPulse(8);
+    Encoder_3.setRatio(46.67);
+    Encoder_3.setPosPid(1.8,0,1.2);
+    Encoder_3.setSpeedPid(0.18,0,0);
+    attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+    Encoder_1.setPulse(8);
+    Encoder_1.setRatio(46.67);
+    Encoder_1.setPosPid(1.8,0,1.2);
+    Encoder_1.setSpeedPid(0.18,0,0);
+    attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+    Encoder_2.setPulse(8);
+    Encoder_2.setRatio(46.67);
+    Encoder_2.setPosPid(1.8,0,1.2);
+    Encoder_2.setSpeedPid(0.18,0,0);
+
+    servo_61.attach(61);
+    servo_60.attach(60);
+    
+    Serial.begin(9600);
+    
+}
+
+void loop()
+{    
+    while(Serial.available()>0)
+  { 
+    serial_get = Serial.read();
+    if(serial_get == ',')
+    {       
+      if (    serNum==0){serSet_0= ser_cache.toInt();}
+      else if(serNum==1){serSet_1= ser_cache.toInt();}
+      else if(serNum==2){serSet_2= ser_cache.toInt();}
+      else if(serNum==3){serSet_3= ser_cache.toInt();}
+      else if(serNum==4){serSet_4= ser_cache.toInt();}
+      else if(serNum==5){serSet_5= ser_cache.toInt();}
+      ser_cache="";
+      serNum = serNum+1;
+      }
+     else if(serial_get == ';')
+      {serNum = 0;      
+       Encoder_1.move((serSet_0*(serSet_2)*2),abs(100));
+       Encoder_2.move((serSet_0*(serSet_3)*2),abs(100));
+       Encoder_3.move((serSet_1*(serSet_4)/10),abs(50));
+
+       servo_61.write(180 - serSet_5);
+       servo_60.write(serSet_5);
+      }
+    else 
+      {ser_cache += char(serial_get);}
+  }
+    _loop();
+}
+
+void _delay(float seconds){
+    long endTime = millis() + seconds * 1000;
+    while(millis() < endTime)_loop();
+}
+
+void _loop(){
+    
+    Encoder_1.loop();
+    
+    Encoder_2.loop();
+
+    Encoder_3.loop();
+    
+    
+}
